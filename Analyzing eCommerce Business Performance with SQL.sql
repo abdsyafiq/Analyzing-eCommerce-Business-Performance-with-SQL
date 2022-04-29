04a9317ac9b6922a8871821a791cac25a8cfed79
Abdurrahman Syafiq I <62006451+abdsyafiq@users.noreply.github.com>
2022-04-29 16:38:16 +0700

    Update Analyzing eCommerce Business Performance with SQL.sql

--- Analyzing eCommerce Business Performance with SQL.sql
+++ Analyzing eCommerce Business Performance with SQL.sql
@@ -1,8 +1,8 @@
 --Data Preparation--
 CREATE TABLE IF NOT EXISTS products (
 	product_id VARCHAR(50) NOT NULL,
-    product_category_name VARCHAR(50),
-    product_name_lenght INT,
+    	product_category_name VARCHAR(50),
+    	product_name_lenght INT,
 	product_description_lenght INT,
 	product_photos_qty INT,
 	product_weight_g INT,
@@ -18,8 +18,8 @@ CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS payments (
 	order_id VARCHAR(50),
-    payment_sequential INT,
-    payment_type VARCHAR(50),
+    	payment_sequential INT,
+    	payment_type VARCHAR(50),
 	payment_installments INT,
 	payment_value DOUBLE PRECISION
 );
@@ -30,8 +30,8 @@ CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS sellers (
 	seller_id VARCHAR(50) NOT NULL,
-    seller_zip_code_prefix INT,
-    seller_city VARCHAR(50),
+    	seller_zip_code_prefix INT,
+    	seller_city VARCHAR(50),
 	seller_state VARCHAR(50),
 	CONSTRAINT sellers_pkey PRIMARY KEY (seller_id)
 );
@@ -42,8 +42,8 @@ CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS order_items (
 	order_id VARCHAR(50),
-    order_item_id INT,
-    product_id VARCHAR(50),
+    	order_item_id INT,
+    	product_id VARCHAR(50),
 	seller_id VARCHAR(50),
 	shipping_limit_date DATE,
 	price DOUBLE PRECISION,
@@ -56,8 +56,8 @@ CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS orders (
 	order_id VARCHAR(50) NOT NULL,
-    customer_id VARCHAR(50),
-    order_status VARCHAR(50),
+    	customer_id VARCHAR(50),
+    	order_status VARCHAR(50),
 	order_purchase_timestamp DATE,
 	order_approved_at DATE,
 	order_delivered_carrier_date DATE,
@@ -72,8 +72,8 @@ CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS reviews (
 	review_id VARCHAR(250),
-    order_id VARCHAR(250),
-    review_score VARCHAR(250),
+    	order_id VARCHAR(250),
+    	review_score VARCHAR(250),
 	review_comment_title VARCHAR(250),
 	review_comment_message VARCHAR(250),
 	review_creation_date VARCHAR(250),
@@ -86,8 +86,8 @@ CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS geolocations (
 	geolocation_zip_code_prefix INT,
-    geolocation_lat DOUBLE PRECISION,
-    geolocation_lng DOUBLE PRECISION,
+    	geolocation_lat DOUBLE PRECISION,
+    	geolocation_lng DOUBLE PRECISION,
 	geolocation_city VARCHAR(50),
 	geolocation_state VARCHAR(50)
 );
@@ -98,8 +98,8 @@ CSV HEADER;
 
 CREATE TABLE IF NOT EXISTS customers (
 	customer_id VARCHAR(50) NOT NULL,
-    customer_unique_id VARCHAR(50),
-    customer_zip_code_prefix INT,
+    	customer_unique_id VARCHAR(50),
+    	customer_zip_code_prefix INT,
 	customer_city VARCHAR(50),
 	customer_state VARCHAR(50),
 	CONSTRAINT customers_pkey PRIMARY KEY (customer_id)
@@ -136,15 +136,15 @@ within the eCommerce platform.*/
 WITH average_customer_active AS (
 	SELECT years,
 	       ROUND(AVG(customer_active)) AS average_customer_active_per_month
-    FROM (
+    	FROM (
 		SELECT DATE_PART('YEAR', t1.order_purchase_timestamp) AS years,
-	           DATE_PART('MONTH', t1.order_purchase_timestamp) AS months,
- 	           COUNT(t2.customer_unique_id) AS customer_active
+	               DATE_PART('MONTH', t1.order_purchase_timestamp) AS months,
+ 	               COUNT(t2.customer_unique_id) AS customer_active
 		FROM orders AS t1
 		LEFT JOIN customers AS t2 ON t2.customer_id = t1.customer_id
 		WHERE order_status != 'canceled'
 		GROUP BY 1,2
-	 	) AS active_customer_per_month
+	     ) AS active_customer_per_month
 	GROUP BY 1
 ),
 new_customer AS (
@@ -152,12 +152,12 @@ new_customer AS (
 	       COUNT(customer_unique_id) AS number_of_new_customer
 	FROM (
 		SELECT t2.customer_unique_id,
-	           MIN(t1.order_purchase_timestamp) AS order_purchase_timestamp
-    	FROM orders AS t1
+	               MIN(t1.order_purchase_timestamp) AS order_purchase_timestamp
+    		FROM orders AS t1
 		LEFT JOIN customers AS t2 ON t2.customer_id = t1.customer_id
 		WHERE order_status != 'canceled'
-    	GROUP BY 1
-		) AS purchase_time
+    		GROUP BY 1
+	     ) AS purchase_time
 	GROUP BY 1
 	ORDER BY 1
 ),
@@ -166,37 +166,37 @@ repeat_orders AS (
 	       COUNT(customer_unique_id) as number_of_customer_repeat_order
 	FROM (	
 		SELECT t1.order_purchase_timestamp,
-	           t2.customer_unique_id,
-	           COUNT(t1.order_id) AS number_of_order
+	               t2.customer_unique_id,
+	               COUNT(t1.order_id) AS number_of_order
 		FROM orders AS t1
 		LEFT JOIN customers AS t2 ON t2.customer_id = t1.customer_id
 		WHERE order_status != 'cancelled'
 		GROUP BY 1,2
 		HAVING COUNT(t1.order_id) > 1
-		) AS repeat_order
+	     ) AS repeat_order
 	GROUP BY 1
 	ORDER BY 1
 ),
 average_number_order_customer AS (
 	SELECT DATE_PART('YEAR', order_purchase_timestamp) AS years,
-           ROUND(AVG(number_of_order)) AS average_number_of_order_per_customer
+               ROUND(AVG(number_of_order)) AS average_number_of_order_per_customer
 	FROM (
 		SELECT t1.order_purchase_timestamp,
-	       	   t2.customer_unique_id,
-	           COUNT(t1.order_id) AS number_of_order
+	       	       t2.customer_unique_id,
+	               COUNT(t1.order_id) AS number_of_order
 		FROM orders AS t1
 		LEFT JOIN customers AS t2 ON t2.customer_id = t1.customer_id
 		WHERE order_status != 'cancelled'
 		GROUP BY 1,2
-		) AS number_order
+	     ) AS number_order
 	GROUP BY 1
 	ORDER BY 1
 )
 SELECT cte1.years,
        cte1.average_customer_active_per_month,
-	   cte2.number_of_new_customer,
-	   cte3.number_of_customer_repeat_order,
-	   cte4.average_number_of_order_per_customer
+       cte2.number_of_new_customer,
+       cte3.number_of_customer_repeat_order,
+       cte4.average_number_of_order_per_customer
 FROM average_customer_active AS cte1
 JOIN new_customer AS cte2 ON cte2.years = cte1.years
 JOIN repeat_orders AS cte3 ON cte3.years = cte1.years
@@ -208,70 +208,70 @@ inside it.*/
 --Annual Product Category Quality Analysis--
 CREATE TEMP TABLE total_revenue AS
 	SELECT DATE_PART('YEAR', order_purchase_timestamp) AS years,
-	   	   ROUND(SUM(price + freight_value)) AS revenue
+	       ROUND(SUM(price + freight_value)) AS revenue
 	FROM (
 		SELECT t1.order_id,
-               t1.order_purchase_timestamp,
-	           t2.price,
-	           t2.freight_value
+                       t1.order_purchase_timestamp,
+	               t2.price,
+	               t2.freight_value
 		FROM orders AS t1
 		LEFT JOIN order_items AS t2 ON t2.order_id = t1.order_id
 		WHERE order_status NOT IN ('unavailable','invoiced','canceled')
-		) as rev
+	     ) AS rev
 	GROUP BY 1;
 
 CREATE TEMP TABLE number_of_order_canceled AS 
 	SELECT DATE_PART('YEAR', order_purchase_timestamp) AS years,
-           COUNT(order_id) AS number_of_order_canceled
+               COUNT(order_id) AS number_of_order_canceled
 	FROM orders
 	WHERE order_status = 'canceled'
 	GROUP BY 1;
 
 CREATE TEMP TABLE revenue_by_category_products_per_year AS
 	SELECT years,
-           product_category_name,
+               product_category_name,
 	       revenue
 	FROM (
 		SELECT DATE_PART('YEAR', t1.order_purchase_timestamp) AS years,
-	           ROUND(SUM(t2.price + t2.freight_value)) AS revenue,
-	           t3.product_category_name,
-	           RANK() OVER (PARTITION BY DATE_PART('YEAR', t1.order_purchase_timestamp)
-					        ORDER BY SUM(t2.price + t2.freight_value) DESC) AS rank_revenue
+	               ROUND(SUM(t2.price + t2.freight_value)) AS revenue,
+	               t3.product_category_name,
+	               RANK() OVER (PARTITION BY DATE_PART('YEAR', t1.order_purchase_timestamp)
+			            ORDER BY SUM(t2.price + t2.freight_value) DESC) AS rank_revenue
 		FROM orders AS t1
 		LEFT JOIN order_items AS t2 ON t2.order_id = t1.order_id
 		LEFT JOIN products AS t3 ON t3.product_id = t2.product_id
 		WHERE order_status NOT IN ('unavailable','invoiced','canceled')
-	  	  	  AND product_category_name IS NOT NULL
+	  	      AND product_category_name IS NOT NULL
 		GROUP BY 1,3
-		) AS rank_revenue_by_category_products_per_year
+	     ) AS rank_revenue_by_category_products_per_year
 	WHERE rank_revenue = 1;
 
 CREATE TEMP TABLE number_of_canceled_by_category_products_per_year AS
 	SELECT years,
-           product_category_name,
+               product_category_name,
 	       number_of_canceled
 	FROM (
 		SELECT DATE_PART('YEAR', t1.order_purchase_timestamp) AS years,
-	           t3.product_category_name,
-	           COUNT(t1.order_status) AS number_of_canceled,
-	           RANK() OVER (PARTITION BY DATE_PART('YEAR', t1.order_purchase_timestamp)
-					        ORDER BY COUNT(t1.order_status) DESC) AS rank_canceled
+	               t3.product_category_name,
+	               COUNT(t1.order_status) AS number_of_canceled,
+	               RANK() OVER (PARTITION BY DATE_PART('YEAR', t1.order_purchase_timestamp)
+			            ORDER BY COUNT(t1.order_status) DESC) AS rank_canceled
 		FROM orders AS t1
 		LEFT JOIN order_items AS t2 ON t2.order_id = t1.order_id
 		LEFT JOIN products AS t3 ON t3.product_id = t2.product_id
 		WHERE order_status = 'canceled'
-	          AND product_category_name IS NOT NULL
+	              AND product_category_name IS NOT NULL
 		GROUP BY 1,2
-		) AS rank_number_of_canceled_by_category_products_per_year
+	     ) AS rank_number_of_canceled_by_category_products_per_year
 	WHERE rank_canceled = 1;
 
 SELECT t1.years,
-	   t1.revenue AS total_revenue,
+       t1.revenue AS total_revenue,
        t2.number_of_order_canceled,
-	   t3.product_category_name AS product_category_with_highest_total_revenue,
-	   t3.revenue AS total_revenue_per_product_category,
-	   t4.product_category_name AS product_category_with_highest_canceled,
-	   t4.number_of_canceled AS number_of_canceled_per_product_category
+       t3.product_category_name AS product_category_with_highest_total_revenue,
+       t3.revenue AS total_revenue_per_product_category,
+       t4.product_category_name AS product_category_with_highest_canceled,
+       t4.number_of_canceled AS number_of_canceled_per_product_category
 FROM total_revenue AS t1
 JOIN number_of_order_canceled AS t2 ON t2.years = t1.years
 JOIN revenue_by_category_products_per_year AS t3 ON t3.years = t1.years
@@ -292,22 +292,22 @@ ORDER BY 2 DESC;
 
 WITH growth_of_payment AS (
 	SELECT payment_type,
-           SUM(CASE WHEN years = 2016 THEN number_of_order ELSE 0 END) AS year_2016,
+               SUM(CASE WHEN years = 2016 THEN number_of_order ELSE 0 END) AS year_2016,
 	       SUM(CASE WHEN years = 2017 THEN number_of_order ELSE 0 END) AS year_2017,
 	       SUM(CASE WHEN years = 2018 THEN number_of_order ELSE 0 END) AS year_2018
 	FROM (
 		SELECT DATE_PART('YEAR', t1.order_purchase_timestamp) AS years,
-               t2.payment_type,
-	           COUNT(t1.order_id) AS number_of_order
+                       t2.payment_type,
+	               COUNT(t1.order_id) AS number_of_order
 		FROM orders AS t1
 		LEFT JOIN payments AS t2 ON t2.order_id = t1.order_id
 		WHERE order_status NOT IN ('unavailable','invoiced','canceled')
-      	      AND payment_type IS NOT NULL
+      	              AND payment_type IS NOT NULL
 		GROUP BY 1,2
-		) AS count_payment
+             ) AS count_payment
 	GROUP BY 1
 )
 SELECT *,
-	   ROUND(((year_2018 - year_2017) / year_2017), 4) * 100 AS growth_rate_2017_2018
+       ROUND(((year_2018 - year_2017) / year_2017), 4) * 100 AS growth_rate_2017_2018
 FROM growth_of_payment
 ORDER BY 5 DESC;
